767512ce4fe5bc40509d135de3c5d877ef954c1b
lyfidyma <102522613+lyfidyma@users.noreply.github.com>
2023-04-12 12:29:24 +0100

    Update Application.java

--- src/main/java/time/prayer/Application.java
+++ src/main/java/time/prayer/Application.java
@@ -1,13 +1,17 @@
 package time.prayer;
 
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class Application {
 
-	public static void main(String[] args){
-		
+	public static void main(String[] args) throws Exception{
+		//PGraph pgr = new PGraph();
+		Scanner scanner = new Scanner(new File("config.txt"));
+		String pgraphClassName = scanner.next();
+		Class pGraph = Class.forName(pgraphClassName);
+		IGraph ipg = (IGraph) pGraph.getDeclaredConstructor().newInstance();
 		System.out.println("Hello");
 	}
 
